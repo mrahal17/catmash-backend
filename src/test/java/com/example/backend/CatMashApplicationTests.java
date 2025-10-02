2077b31a61ff2c6ae9a669307763cd358774ddfd
mrahal17 <maya.rahal17@gmail.com>
2025-10-02 14:19:50 +0000

    Remove dummy test and rename file

--- src/test/java/com/example/backend/CatMashApplicationTests.java
+++ src/test/java/com/example/backend/CatMashApplicationTests.java
@@ -2,18 +2,12 @@ package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CatMashApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	void dummyTest() {
-		assertEquals(2, 2, "Test pipeline");
 	}
 
 }
