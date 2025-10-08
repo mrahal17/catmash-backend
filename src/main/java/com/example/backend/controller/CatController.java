c156b7125b2672d87243e33fbc91c01061bd20dc
mrahal17 <maya.rahal17@gmail.com>
2025-10-08 18:30:10 +0000

    Provide CORS config

--- src/main/java/com/example/backend/controller/CatController.java
+++ src/main/java/com/example/backend/controller/CatController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Map;
 
+@CrossOrigin(origins = "https://mrahal17.github.io")
 @RestController
 @RequestMapping("/api/cats")
 public class CatController {
